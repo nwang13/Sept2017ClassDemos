62892f4f246b1ffd10cadc86b932d56229e459c6
nwang13 <nwang13@studentmail.nait.ca>
2017-11-15 20:42:37 -0700

    add up, down button

--- ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
+++ ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
@@ -143,6 +143,87 @@ namespace ChinookSystem.BLL
             {
                 //code to go here 
 
+                var exists = (from x in context.Playlists
+                              where x.UserName.Equals(username)
+                              && x.Name.Equals(playlistname)
+                              select x).FirstOrDefault();
+                if(exists == null)
+                {
+                    throw new Exception("Play List has been removed from the file");
+                }
+                else
+                {
+                    PlaylistTrack moveTrack = (from x in exists.PlaylistTracks
+                                               where x.TrackId == trackid
+                                               select x).FirstOrDefault();
+
+                    if (moveTrack == null)
+                    {
+                        throw new Exception("Play List track has been removed from the file");
+                    }
+                    else
+                    {
+                        PlaylistTrack otherTrack = null;
+
+                        if(direction.Equals("up"))
+                        {
+                            //up
+                            if(moveTrack.TrackNumber == 1)
+                            {
+                                throw new Exception("Play List track already at top");
+                            }
+                            else
+                            {
+                                otherTrack = (from x in exists.PlaylistTracks
+                                              where x.TrackNumber == moveTrack.TrackNumber - 1
+                                              select x).FirstOrDefault();
+                                if(otherTrack == null)
+                                {
+                                    throw new Exception("Other play List track is missing");
+                                }
+                                else
+                                {
+                                    moveTrack.TrackNumber -= 1;
+                                    otherTrack.TrackNumber += 1; 
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //down
+
+                            if (moveTrack.TrackNumber == exists.PlaylistTracks.Count)
+                            {
+                                throw new Exception("Play List track already at bottom");
+                            }
+                            else
+                            {
+                                otherTrack = (from x in exists.PlaylistTracks
+                                              where x.TrackNumber == moveTrack.TrackNumber + 1
+                                              select x).FirstOrDefault();
+                                if (otherTrack == null)
+                                {
+                                    throw new Exception("Other play List track is missing");
+                                }
+                                else
+                                {
+                                    moveTrack.TrackNumber += 1;
+                                    otherTrack.TrackNumber -= 1;
+                                }
+                            }
+                        }//eof up/down
+
+                        //staging
+
+                        context.Entry(moveTrack).Property(y => y.TrackNumber).IsModified = true;
+                        context.Entry(otherTrack).Property(y => y.TrackNumber).IsModified = true;
+
+                        //saving
+
+                        context.SaveChanges();
+                    }
+                }
+
             }
         }//eom
 
